cdf98040b13e82f7a468e55dd0aa7566854b0bf8
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2009-11-19 13:34:53 +0100

    Replaced user model functions with model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,3 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
-  
-  validates_presence_of :email
-  
-  def deliver_email_confirmation_instructions!  
-    reset_perishable_token!  
-    NineAuthMailer.deliver_email_confirmation_instructions(self)  
-  end
-
-  def deliver_password_reset_instructions!  
-    reset_perishable_token!  
-    NineAuthMailer.deliver_password_reset_instructions(self)  
-  end
-  
-  def deliver_account_created_instructions(password)!  
-    reset_perishable_token!  
-    NineAuthMailer.deliver_account_created_instructions(self, password)  
-  end
-  
+  include NineAuthEngine::User
 end
